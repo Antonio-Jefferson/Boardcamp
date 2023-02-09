dbc597ea698e1d41e18187f01dec922b0fc80248
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-02-09 15:31:53 -0300

    feat: função de buscar cliente pelo id inserida e upedete do cliente inserida também

--- src/controllers/Customers.controller.js
+++ src/controllers/Customers.controller.js
@@ -22,8 +22,28 @@ const getCustomers = async (req, res)=>{
         res.status(500).send(error.message)
     }
 }
-const upCustomers = (req, res)=>{
-    
+const getCustomerId = async(req, res)=>{
+    const {id} = req.params
+    try {
+        const promise = await db.query(`SELECT * FROM customers WHERE id = $1`, [id])
+        res.status(200).send(promise.rows)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+const upCustomers = async (req, res)=>{
+    const {name, phone, cpf, birthday} = req.body
+    const {id} = req.params
+    try {
+        const result = await db.query(`SELECT * FROM customers WHERE cpf = $1`, [cpf])
+        if(result.rowCount > 0) return res.status(409).send()
+
+        await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5`, [name, phone, cpf, birthday, id])
+        res.status(200).send()
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 }
 
-export {createCustomers, getCustomers, upCustomers}
+export {createCustomers, getCustomers, upCustomers,getCustomerId}
--- src/routers/CustomersRouter.js
+++ src/routers/CustomersRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { createCustomers, upCustomers, getCustomers } from "../controllers/Customers.controller.js";
+import { createCustomers, upCustomers, getCustomers, getCustomerId } from "../controllers/Customers.controller.js";
 import { validatSchemas } from "../middlewares/validationSchema.js";
 import { validationCustomer } from "../schemas/customersSchema.js";
 const customersRouter = Router();
 
 customersRouter.post("/customers",validatSchemas(validationCustomer),createCustomers);
 customersRouter.get("/customers",getCustomers);
-customersRouter.get("/customers/:id",getCustomers)
-customersRouter.patch("/customers/:id",upCustomers)
+customersRouter.get("/customers/:id",getCustomerId)
+customersRouter.put("/customers/:id",validatSchemas(validationCustomer),upCustomers)
 
 
 export default customersRouter;
