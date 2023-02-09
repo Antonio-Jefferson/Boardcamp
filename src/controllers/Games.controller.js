f4e7bbb1ee832c16e70832002cfb84b7e43fb294
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-02-09 14:27:40 -0300

    fixd: erros de status corrigidos

--- src/controllers/Games.controller.js
+++ src/controllers/Games.controller.js
@@ -1,26 +1,30 @@
-import {db} from "../database/database.js"
+import { db } from "../database/database.js"
 
-const creatGamer = async (req, res)=>{
-    const {name, image, stockTotal, pricePerDay} = req.body
+const creatGamer = async (req, res) => {
+    const { name, image, stockTotal, pricePerDay } = req.body
     try {
-        const result = await db.query(`SELECT * FROM games WHERE name = '${name}'`);
-        console.log(result.rows)
-        if(result) res.status(409).send({message: "Nome do jogo já existe"})
+        const result = await db.query(`SELECT * FROM games WHERE name = $1`, [name]);
         
-        const promise = await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`,[name, image, stockTotal, pricePerDay])
-        if(promise.rowCount > 0) return res.status(201).send({message: "Gamer created successfully"})
-       
+        if (result.rows.length > 0) {
+            res.status(409).send({ message: "Nome do jogo já existe" })
+        } else {
+            await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay])
+            res.status(201).send({ message: "Gamer created successfully" })
+        }
+
     } catch (error) {
         res.status(500).send(error.message)
     }
 }
-const getGamer = async (req, res)=>{
+
+
+const getGamer = async (req, res) => {
     try {
         const promise = await db.query("SELECT * FROM games")
         res.status(200).send(promise.rows)
     } catch (error) {
         res.status(500).send(error.message)
-}
+    }
 }
 
-export {creatGamer, getGamer};
+export { creatGamer, getGamer };
