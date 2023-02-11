89733ba53675da85943712270f2777d385ff18c9
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-02-11 18:47:21 -0300

    fixed: corrigindo retorno de status e emplementando outras validações em createRet

--- src/controllers/Rent.controller.js
+++ src/controllers/Rent.controller.js
@@ -8,18 +8,24 @@ const createRet = async (req, res) => {
     const delayFee = null;
 
     try {
-        const customer = await db.query(`SELECT * FROM customers WHERE id=$1`, [customerId]);
-        const isAvailable = await db.query(`SELECT * FROM rentals WHERE "gameId"=$1`, [gameId]);
+        if(parseInt(daysRented) <= 0) return res.status(400)
+        const customer = await db.query(`SELECT * FROM customers WHERE id = $1`, [customerId]);
+        const isAvailable = await db.query(`SELECT * FROM games WHERE id = $1`, [gameId]);
         const stock = await db.query(`SELECT games."stockTotal" FROM games WHERE id=$1`, [gameId]);
 
         if (!customer.rows.length > 0 && !isAvailable.rows.length > 0) return res.sendStatus(400)
         if (stock === 0) return res.sendStatus(400)
+        const isGameId = await connection.query(`SELECT * FROM rentals WHERE "gameId"=$1 AND "returnDate" is null`,[gameId]);
+        const stockTotal = game.rows[0].stockTotal;
+    
+        if (stockTotal -isGameId.rowCount === 0) return res.sendStatus(400);
 
         const pricePerDay = await db.query(`SELECT games."pricePerDay" FROM games WHERE id=$1`, [gameId]);
-
         const originalPrice = pricePerDay.rows[0].pricePerDay * daysRented;
+
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)`,[customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee,]);
         res.status(201).send("aluguel criado");
+
     } catch (error) {
         res.satus(500);
     }
--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -4,6 +4,6 @@ export const validationCustomer = JOI.object({
     name:JOI.string().required(), 
     phone:JOI.string().min(10).max(11).required(), 
     cpf:JOI.string().min(11).max(11).regex(/^\d+$/).required(), 
-    birthday:JOI.string().required()
+    birthday: JOI.string().pattern(/^\d{4}\-\d{2}\-\d{2}$/).required()
 })
 
