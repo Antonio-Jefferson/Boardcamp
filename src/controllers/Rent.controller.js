253902dbfab29c956be7959144177436d9969ddd
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-02-12 23:57:04 -0300

    corrigido o bug

--- src/controllers/Rent.controller.js
+++ src/controllers/Rent.controller.js
@@ -61,25 +61,22 @@ const upRet = async (req, res) => {
     try {
         const isAvailable = await db.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
 
-        if (isAvailable.rowCount === 0){
+        if (isAvailable.rowCount === 0) {
             return res.status(404).send("error")
-        } 
-
-        const result = await db.query(`SELECT rentals.*, games."pricePerDay" AS "pricePerDay" FROM rentals JOIN games ON games.id=rentals."gameId" WHERE rentals.id=$1`, [id]);
-        const rental = result.rows[0];
-
-        if (rental.returnDate){
-            return res.status(400).send("error")
         }
-    
-        const rentalDueDate = dayjs(rental.dueDate);
-        const returnDate = dayjs(req.body.returnDate);
-        const delayDays = returnDate.diff(rentalDueDate, "days");
-        const delayFee = delayDays > 0 ? parseInt(delayDays) * rental.pricePerDay : 0;
+        if (isAvailable.rows[0].returnDate !== null) {
+            res.sendStatus(400);
+        }
 
-        await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3`, [returnDate.toISOString(), delayFee, id]);
+        const result = await db.query( `SELECT rentals.*, games."pricePerDay" AS "pricePerDay" FROM rentals JOIN games ON games id=rentals."gameId" WHERE rentals.id=$1`, [id]);
 
-        res.status(200)
+        const delayDays = dayjs().diff(result.rows[0], "days");
+
+        const delayFee = delayDays > 0 ? parseInt(delayDays) * result.rows[0].pricePerDay : 0;
+
+        await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3`,[returnDate, delayFee, id]);
+
+        res.sendStatus(200);
     } catch (error) {
         res.status(500).send(error.message);
     }
