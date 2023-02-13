728646e2155a12e50a41221c2e51241d3376cf14
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-02-13 00:51:11 -0300

    delete algueis

--- src/controllers/Rent.controller.js
+++ src/controllers/Rent.controller.js
@@ -59,60 +59,61 @@ const upRet = async (req, res) => {
     const { id } = req.params;
     const returnDate = dayjs().format("YYYY-MM-DD HH:mm");
 
-try {
-    const isAvailable = await db.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
+    try {
+        const isAvailable = await db.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
 
-    if (isAvailable.rowCount === 0) {
-        return res.status(404).send("error");
-    }
-    if (isAvailable.rows[0].returnDate !== null) {
-        res.status(400).send("error");
-    }
+        if (isAvailable.rowCount === 0) {
+            return res.status(404).send("error");
+        }
+        if (isAvailable.rows[0].returnDate !== null) {
+            res.status(400).send("error");
+        }
 
-    const result = await db.query(`
+        const result = await db.query(`
         SELECT rentals.*, games."pricePerDay" AS "pricePerDay"
         FROM rentals
         JOIN games ON games."id" = rentals."gameId"
         WHERE rentals.id = $1
     `, [id]);
 
-    const dueDate = result.rows[0].dueDate;
-    const pricePerDay = result.rows[0].pricePerDay;
+        const dueDate = result.rows[0].dueDate;
+        const pricePerDay = result.rows[0].pricePerDay;
 
-    let delayFee = 0;
-    if (dayjs(returnDate).isAfter(dueDate)) {
-        const delayDays = dayjs(returnDate).diff(dueDate, "days");
-        delayFee = delayDays * pricePerDay;
-    }
+        let delayFee = 0;
+        if (dayjs(returnDate).isAfter(dueDate)) {
+            const delayDays = dayjs(returnDate).diff(dueDate, "days");
+            delayFee = delayDays * pricePerDay;
+        }
 
-    await db.query(`
+        await db.query(`
         UPDATE rentals
         SET "returnDate" = $1, "delayFee" = $2
         WHERE id = $3
     `, [returnDate, delayFee, id]);
 
-    res.status(200).send("success");
-} catch (error) {
-    res.status(500).send("error");
-}
+        res.status(200).send("success");
+    } catch (error) {
+        res.status(500).send("error");
+    }
 
 }
 const deleteRet = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const result = await db.query(`SELECT * FFROM rentals id = $1`, [id])
-        if (result.rowCount === 0) return res.status(404).send("error")
+        const result = await db.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
+        if (result.rowCount === 0) return res.status(404).send("error");
 
-        const res = await db.query('SELECT * FROM rentals WHERE id = $1 AND "returnDate" IS NOT NULL', [id])
-        if (!res.rowCount > 0) return res.status(400).send("error");
+        const rental = await db.query('SELECT * FROM rentals WHERE id = $1 AND "returnDate" IS NOT NULL', [id]);
+        if (rental.rowCount === 0) return res.status(400).send("error");
 
-        await db.query(`DELETE FROM rentals WHERE id=$1`, [id]);
-        res.status(200).send("sucesso");
+        await db.query(`DELETE FROM rentals WHERE id = $1`, [id]);
+        res.status(200).send("success");
 
     } catch (error) {
         res.status(500).send(error.message);
     }
+
 }
 
 export { createRet, upRet, deleteRet, getRet };
